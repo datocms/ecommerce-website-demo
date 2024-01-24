1f2740503cd7edfd96d4920fcc95b8d9c819bf05
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2024-01-24 17:39:46 +0100

    Global seo

--- app/[lng]/Head.tsx
+++ app/[lng]/Head.tsx
@@ -1,13 +1,25 @@
-export default function Head() {
+import queryDatoCMS from '@/utils/queryDatoCMS';
+import { getFallbackLocale } from '../i18n/settings';
+import { MetaDocument, SiteLocale } from '@/graphql/generated';
+
+export default async function Head() {
+  const fallbackLng = await getFallbackLocale();
+  const data = await queryDatoCMS(MetaDocument, {
+    fallbackLocale: [fallbackLng],
+  });
+
   return (
     <>
-      <title>Landing page DatoCMS Starter</title>
+      <title>{data._site.globalSeo?.fallbackSeo?.title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta
         name="description"
-        content="Visit https://www.datocms.com/marketplace/starters for more starters"
+        content={
+          data._site.globalSeo?.fallbackSeo?.description ||
+          'Visit https://www.datocms.com/marketplace/starters for more starters'
+        }
       />
-      <link rel="icon" href="/images/favicon.ico" />
+      <link rel="icon" href={data._site.favicon?.url} />
     </>
   );
 }
